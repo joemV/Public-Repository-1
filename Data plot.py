c6a6d194637f03740778392776dfe710b466d9fc
Joe W Mwangi <32553604+joemV@users.noreply.github.com>
2018-07-22 00:28:46 +0300

    Completed Perceptron Neuron
    The added code is the final part of the perceptron neuron that will allow for the complete working of the system as a whole.

--- Data plot.py
+++ Data plot.py
@@ -38,6 +38,17 @@ class perceptron(object):
     self.w_ = np.zeros(1 + X.shape[1])
     self.errors_ = []
     for _ in range(self.n_iter):
-      errors = 0
+      error = 0
       for xi, target in zip(X, y):
         update = self.eta * (target - self.predict(xi))
+        self.w_[1:] += update * xi
+        self.w_[0] += update
+        error += int(update != 0.0)
+      self.errors_.append(error)
+    return self
+	
+	def predict(self, X):
+		return np.where(self.net_input(X) >= 0.0, 1, -1)
+	
+	def net_input(self, X):
+		return np.dot(X, self.w_[1:]) + self.w_[0]
